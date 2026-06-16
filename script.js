211e639f2d35a856e76498a5f1fb8ec6f7cf3894
AlexUccello <dicemonth227alt@gmail.com>
2026-06-16 12:33:12 +0200

    titolo gioco = link

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function cerca(){
     data.forEach(game => {
         html += "<div class='game' onclick=\"clickGioco("+ game.gameID + ")\">";
         html += "<img src=" + game.thumb + " class=\"thumb\">";
-        html += "<a href=\"https://www.cheapshark.com/redirect?dealID=" + game.cheapestDealID + "\" class=\"title\">" + game.external + "</a>";
+        html += "<a href=\"https://www.cheapshark.com/redirect?dealID=" + game.cheapestDealID + "\" class=\"title\" target=\"_blank\">" + game.external + "</a>";
         html += "<p class=\"price\">Prezzo: " + game.cheapest + "</p>";
         html += "</div>";
     });
