f61a047acb13e87c8dde6b002df486cc07d993b3
Francesco15012010 <francesco.murgia.s2@giua.edu.it>
2026-06-17 11:43:26 +0200

    Aggiunto messaggio "Gioco non trovato"

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ async function cerca(){
  var input = document.getElementById("searchInput").value;
  if(input){
     try {
+
         document.querySelector(".loader").style.display = "block";
         document.getElementById("results").innerHTML = "";
 
         var response = await fetch("https://www.cheapshark.com/api/1.0/games?title=" + input);
         const data = await response.json();
+        let html = "";
 
         if(data.length < 1){
-            alert("nessun gioco");
-            document.getElementById("results").innerHTML = "";
+            html += "<div class='notfound'>Nessun gioco trovato</div>";
+            document.getElementById("results").innerHTML = html;
         } else {
-            let html = "";
             data.forEach(game => {
                 html += "<div class='game' onclick=\"clickGioco("+ game.gameID + ")\">";
                 html += "<img src=" + game.thumb + " class=\"thumb\">";
